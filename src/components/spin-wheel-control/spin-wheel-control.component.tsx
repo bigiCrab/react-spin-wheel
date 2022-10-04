8e2910cf4850eed9b33e0c41ccf76f1fab05093d
BigiCrab <jerry850408@gmail.com>
2022-10-04 08:01:46 +0800

    add wheel control style

--- src/components/spin-wheel-control/spin-wheel-control.component.tsx
+++ src/components/spin-wheel-control/spin-wheel-control.component.tsx
@@ -25,63 +25,66 @@ const SpinWheelControl = ({
 }) => {
   return (
     <div className="spin-wheel-control">
-      {prizes.map((prize, index) => (
-        <div
-          key={index}
-          style={{
-            color: prize.color,
-            backgroundColor: prize.bgColor,
-          }}
-        >
-          <span
+      <div className="prizes-container">
+        {prizes.map((prize, index) => (
+          <div
+            key={index}
+            className="prize-row"
             style={{
-              display: "inline-block",
-              width: "3em",
-              textAlign: "center",
+              color: prize.color,
+              backgroundColor: prize.bgColor,
             }}
           >
-            {index === landOnIdx ? (
-              "✨"
-            ) : (
-              <button
-                aria-label="change land on button"
-                style={{ fontSize: "0.5em" }}
-                onClick={() => onLandOnIdxChangeHandler(index)}
-              >
-                PICK
-              </button>
-            )}
-          </span>
-          <span>
-            name:
-            {/* TODO implement ReactLink */}
-            <input
-              aria-label="prizes name input box"
-              type="text"
-              style={{ maxWidth: "4.5rem" }}
-              value={prize.name}
-              onChange={(event) => onPrizeNameChangeHandler(event, index)}
-            />
-          </span>
-          <span>
-            proportion:
-            <input
-              aria-label="proportion input box"
-              type="number"
-              style={{ maxWidth: "2.5rem" }}
-              value={prize.proportion}
-              min="0.01"
-              onChange={(event) => onProportionChangeHandler(event, index)}
-            />
-          </span>
-          <button
-            aria-label="delete button"
-            onClick={() => onDeleteHandler(prize)}
-          >
-            -
-          </button>
-        </div>
-      ))}
+            <span
+              style={{
+                display: "inline-block",
+                width: "4.5em",
+                textAlign: "center",
+              }}
+            >
+              {index === landOnIdx ? (
+                "✨"
+              ) : (
+                <button
+                  aria-label="change land on button"
+                  // style={{ fontSize: "0.5em" }}
+                  onClick={() => onLandOnIdxChangeHandler(index)}
+                >
+                  PICK
+                </button>
+              )}
+            </span>
+            <span>
+              name:
+              {/* TODO implement ReactLink */}
+              <input
+                aria-label="prizes name input box"
+                type="text"
+                style={{ maxWidth: "4.5rem" }}
+                value={prize.name}
+                onChange={(event) => onPrizeNameChangeHandler(event, index)}
+              />
+            </span>
+            <span>
+              proportion:
+              <input
+                aria-label="proportion input box"
+                type="number"
+                style={{ maxWidth: "2.5rem" }}
+                value={prize.proportion}
+                min="0.01"
+                onChange={(event) => onProportionChangeHandler(event, index)}
+              />
+            </span>
+            <button
+              aria-label="delete button"
+              onClick={() => onDeleteHandler(prize)}
+            >
+              -
+            </button>
+          </div>
+        ))}
+      </div>
       <button
         aria-label="reset button"
         onClick={onResetHandler}
